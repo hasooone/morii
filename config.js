a32cb6417b9f431856ba806342ad3b44fead9153
hasan <hasan@example.com>
2026-06-08 07:52:34 +0300

    Fix config for Railway

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
-const config = {
-  token: process.env.TOKEN,
-  clientId: process.env.CLIENT_ID,
-  guildId: process.env.GUILD_ID,
-  inputChannelId: process.env.INPUT_CHANNEL_ID,
-  outputChannelId: process.env.OUTPUT_CHANNEL_ID,
-};
+let local = {};
+try {
+  local = require('./config.json');
+} catch {}
 
-const missing = Object.entries(config).filter(([, v]) => !v);
-if (missing.length) {
-  const local = require('./config.json');
-  Object.assign(config, local);
-}
+const config = {
+  token: process.env.TOKEN || local.token,
+  clientId: process.env.CLIENT_ID || local.clientId,
+  guildId: process.env.GUILD_ID || local.guildId,
+  inputChannelId: process.env.INPUT_CHANNEL_ID || local.inputChannelId,
+  outputChannelId: process.env.OUTPUT_CHANNEL_ID || local.outputChannelId,
+};
 
 module.exports = config;
